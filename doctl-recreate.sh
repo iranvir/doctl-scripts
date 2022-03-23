8c4ed42cc6067470754f329e42c8be89e470d9fd
Ranvir Singh <r@ranvir.tech>
2022-03-23 09:09:11 +0000

    Add logic to delete known_hosts

--- doctl-recreate.sh
+++ doctl-recreate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 doctl compute droplet list --tag-name galera --no-header --format=ID | xargs -I vm doctl compute droplet-action rebuild --image debian-11-x64 vm
+rm $HOME/.ssh/known_hosts
 exit 0
